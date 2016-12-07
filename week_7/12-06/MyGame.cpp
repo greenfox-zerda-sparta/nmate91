ced1adb74beff3d3af17462ad0d8958a9ff4f8f8
nmate91 <nmate91@gmail.com>
2016-12-07 15:36:41 +0100

    random map is ok, player is rollin

---
+++ week_7/12-06/MyGame.cpp
@@ -0,0 +1,127 @@
+#include "MyGame.h"
+#include <time.h>
+
+MyGame::MyGame() {
+  srand(time(NULL));
+  std::vector<std::vector<int>> vic(10, std::vector<int>(10, 1));
+  this->v = vic;
+  int random_number = 0;
+  for (int i = 1; i < v.size() - 1; i++) {
+    for (int j = 1; j < v[i].size() - 1; j++) {
+      random_number = rand() % 2;
+      v[i][j] = random_number;
+    }
+  }
+  for (int i = 1; i < v.size() - 1; i++) {
+    for (int j = 1; j < v[i].size() - 1; j++) {
+      if (v[i - 1][j - 1] == 0) {
+        v[i][j] = 1;
+      }
+      else if (v[i][j - 1] == 1 && v[i - 1][j] == 1 && v[i][j + 1] == 1) {
+        v[i][j] = 0;
+      }
+      else if (v[i][j - 1] == 0 && v[i + 1][j] == 0 && v[i - 1][j] == 0 && v[i][j + 1] == 0) {
+        v[i][j] = 0;
+      }
+      else if ((v[i][j - 1] == 1 && v[i + 1][j] == 1) || (v[i - 1][j] == 1 && v[i][j + 1] == 1)) {
+        v[i][j] = 1;
+      }
+    }
+  }
+}
+
+MyGame::~MyGame() {
+}
+
+void MyGame::init(GameContext& context) {
+  context.load_file("floor.bmp");
+  context.load_file("wall.bmp");
+  context.load_file("hero-down.bmp");
+  context.load_file("hero-up.bmp");
+  context.load_file("hero-left.bmp");
+  context.load_file("hero-right.bmp");
+}
+
+void MyGame::draw_char_direction(GameContext& context, int keycode) {
+  switch (keycode) {
+  case 0:
+    context.draw_sprite("hero-up.bmp", x, y);
+    break;
+  case 1:
+    context.draw_sprite("hero-right.bmp", x, y);
+    break;
+  case 2:
+    context.draw_sprite("hero-down.bmp", x, y);
+    break;
+  case 3:
+    context.draw_sprite("hero-left.bmp", x, y);
+    break;
+  }
+}
+
+void MyGame::render(GameContext& context) {
+  for (int i = 0; i < v.size(); i++) {
+    for (int j = 0; j < v[i].size(); j++) {
+      if (v[i][j] == 0) {
+        context.draw_sprite("wall.bmp", i * 72, j * 72);
+      }
+      else {
+        context.draw_sprite("floor.bmp", i * 72, j * 72);
+      }
+    }
+  }
+  draw_char_direction(context, keycode);
+  if (context.was_key_pressed(ARROW_DOWN)) {
+    y = y + 72;
+    if (y > (720 - 72)) {
+      y = (720 - 72);
+      context.render();
+    }
+    else if (v[x / 72][y / 72] == 0) {
+      y = y - 72;
+    }
+    context.reset_keys();
+    context.on_key_down(ARROW_DOWN);
+    keycode = 2;
+  }
+  else if (context.was_key_pressed(ARROW_UP)) {
+    y = y - 72;
+    if (y < 0) {
+      y = 0;
+      context.render();
+    }
+    else if (v[x / 72][y / 72] == 0) {
+      y = y + 72;
+    }
+    context.reset_keys();
+    context.on_key_down(ARROW_UP);
+    keycode = 0;
+  }
+  else if (context.was_key_pressed(ARROW_LEFT)) {
+    x = x - 72;
+    if (x < 0) {
+      x = 0;
+      context.render();
+    }
+    else if (v[x / 72][y / 72] == 0) {
+      x = x + 72;
+    }
+    context.reset_keys();
+    context.on_key_down(ARROW_LEFT);
+    keycode = 3;
+  }
+  else if (context.was_key_pressed(ARROW_RIGHT)) {
+    x = x + 72;
+    if (x > (720 - 72)) {
+      x = (720 - 72);
+      context.render();
+    }
+    else if (v[x / 72][y / 72] == 0) {
+      x = x - 72;
+    }
+    context.reset_keys();
+    context.on_key_down(ARROW_RIGHT);
+    keycode = 1;
+  }
+  context.render();
+}
--- week_7/12-06/MyGame.h
+++ week_7/12-06/MyGame.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "GameEngine.h"
 
 class MyGame : public Game {
@@ -6,96 +7,12 @@ private:
   std::vector<std::vector<int>> v;
   int x = 0;
   int y = 0;
-  int keycode;
+  int keycode = 2;
 public:
-  MyGame() {
-    std::vector<std::vector<int>> vic(10, std::vector<int>(10, 100));
-    this->v = vic;
-    int random_number = 0;
-    for (int j = 0; j < v.size(); j++) {
-      for (int i = 0; i < v[j].size(); i++) {
-        random_number = rand() % 100 + 1;
-        v[j][i] = random_number;
-      }
-    }
-  }
-  ~MyGame() {
-  }
-
-  virtual void init(GameContext& context) {
-    context.load_file("floor.bmp");
-    context.load_file("wall.bmp");
-    context.load_file("hero-down.bmp");
-    context.load_file("hero-up.bmp");
-    context.load_file("hero-left.bmp");
-    context.load_file("hero-right.bmp");
-  }
-
-  virtual void render(GameContext& context) {
-    for (int i = 0; i < v.size(); i++) {
-      for (int j = 0; j < v[i].size(); j++) {
-        if (v[i][j] % 5 == 0) {
-          context.draw_sprite("wall.bmp", i * 72, j * 72);
-        }
-        else {
-          context.draw_sprite("floor.bmp", i * 72, j * 72);
-        }
-      }
-    }
-    context.draw_sprite("hero-down.bmp", x, y);
-    if (context.was_key_pressed(ARROW_DOWN)) {
-      y = y + 72;
-      if (y > (720 - 72)) {
-        y = (720 - 72);
-        context.render();
-      }
-      else if (v[x / 72][y / 72] % 5 == 0) {
-        y = y - 72;
-      }
-      context.reset_keys();
-      context.on_key_down(ARROW_DOWN);
-      context.draw_sprite("hero-down.bmp", x, y);
-    }
-    else if (context.was_key_pressed(ARROW_UP)) {
-      y = y - 72;
-      if (y < 0) {
-        y = 0;
-        context.render();
-      }
-      else if (v[x / 72][y / 72] % 5 == 0) {
-        y = y + 72;
-      }
-      context.reset_keys();
-      context.on_key_down(ARROW_UP);
-      context.draw_sprite("hero-up.bmp", x, y);
-    }
-    else if (context.was_key_pressed(ARROW_LEFT)) {
-      x = x - 72;
-      if (x < 0) {
-        x = 0;
-        context.render();
-      }
-      else if (v[x / 72][y / 72] % 5 == 0) {
-        x = x + 72;
-      }
-      context.reset_keys();
-      context.on_key_down(ARROW_LEFT);
-      context.draw_sprite("hero-left.bmp", x, y);
-    }
-    else if (context.was_key_pressed(ARROW_RIGHT)) {
-      x = x + 72;
-      if (x > (720 - 72)) {
-        x = (720 - 72);
-        context.render();
-      }
-      else if (v[x / 72][y / 72] % 5 == 0) {
-        x = x - 72;
-      }
-      context.reset_keys();
-      context.on_key_down(ARROW_RIGHT);
-      context.draw_sprite("hero-right.bmp", x, y);
-    }
-    context.render();
-  }
+  MyGame();
+  ~MyGame();
+  virtual void draw_char_direction(GameContext& context, int keycode);
+  virtual void init(GameContext& context);
+  virtual void render(GameContext& context);
 };
 
