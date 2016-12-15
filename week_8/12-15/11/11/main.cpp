05f0492a19c5eb8249222a7df167c48c4cfcc0d5
nmate91 <nmate91@gmail.com>
2016-12-15 17:03:49 +0100

    STARWAAAAAAARS

--- week_8/12-15/11/11/main.cpp
+++ week_8/12-15/11/11/main.cpp
@@ -2,12 +2,24 @@
 #include "draw.h"
 #include <SDL.h>
 
+void draw_something(draw& d, SDL_Renderer* renderer, int x, int y) {
+  if (x < 600 && y < 600 && x < y) {
+    d.DrawTo(renderer, x + 200, y);
+    return draw_something(d, renderer, x + 200, y);
+  } 
+  else if (x < 600 && y < 600 && x == y) {
+    d.DrawTo(renderer, x, y + 200);
+    return draw_something(d, renderer, x, y + 200);
+  }
+  return;
+}
+
 int main(int argc, char ** argv) {
   bool quit = false;
   SDL_Event event;
   SDL_Init(SDL_INIT_EVERYTHING);
   SDL_Window* window = SDL_CreateWindow("SDL2 Fractal",
-    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 600, 0);
   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
   while (!quit) {
@@ -21,7 +33,7 @@ int main(int argc, char ** argv) {
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     draw d;
-    d.DrawTo(renderer, 200, 200);
+    draw_something(d, renderer, 0, 0);
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     SDL_RenderPresent(renderer);
   }
