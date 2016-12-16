0e8e5f613eb2994e4f2ecbd93d90a836dcece478
nmate91 <nmate91@gmail.com>
2016-12-16 09:54:20 +0100

    11 is working with pink lines STARWAAARS

--- week_8/12-15/11/11/main.cpp
+++ week_8/12-15/11/11/main.cpp
@@ -3,20 +3,20 @@
 #include <SDL.h>
 
 void draw_something(draw& d, SDL_Renderer* renderer, int x_i, int y_i, int x, int y) {
-  if (x_i > 2) {
-
+  if (x_i >= 2 && y_i >= 2) {
     d.MoveTo(x + x_i / 3, y);
     d.DrawTo(renderer, x + x_i / 3, y + y_i);
-
     d.MoveTo(x + 2 * x_i / 3, y);
     d.DrawTo(renderer, x + 2 * x_i / 3, y + y_i);
-    
     d.MoveTo(x, y + y_i / 3);
     d.DrawTo(renderer, x + x_i, y + y_i / 3);
-    
     d.MoveTo(x, y + 2 * y_i / 3);
     d.DrawTo(renderer, x + x_i, y + 2 * y_i / 3);
-  }
+    draw_something(d, renderer, x_i / 3, y_i / 3, x + x_i / 3, y);
+    draw_something(d, renderer, x_i / 3, y_i / 3, x + x_i / 3, y + 2 * y_i / 3);
+    draw_something(d, renderer, x_i / 3, y_i / 3, x + 2 * x_i / 3, y + y_i / 3);
+    draw_something(d, renderer, x_i / 3, y_i / 3, x, y + y_i / 3);
+  } 
   return;
 }
 
@@ -27,7 +27,7 @@ int main(int argc, char ** argv) {
   SDL_Window* window = SDL_CreateWindow("SDL2 Fractal",
     SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 600, 0);
   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+  SDL_SetRenderDrawColor(renderer, 255, 20, 147, 0);
   while (!quit) {
     SDL_WaitEvent(&event);
     switch (event.type) {
@@ -35,9 +35,9 @@ int main(int argc, char ** argv) {
       quit = true;
       break;
     }
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
     SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
 
     draw d;
     draw_something(d, renderer, 600, 600, 0, 0);
