a4fd2adab7b750a109bbfa2713a83417e9d17417
nmate91 <nmate91@gmail.com>
2016-12-19 16:11:52 +0100

    getS, setS added

--- week_9/12-19/forth_test/forth_test/forth_test.cpp
+++ week_9/12-19/forth_test/forth_test/forth_test.cpp
@@ -42,9 +42,14 @@ TEST_CASE("lofasz", "[anyad]") {
   REQUIRE_FALSE(bizbasz.isAnAnagram("das    ghf") != true);
 }
 
-TEST_CASE("lofasz", "[anyad]") {
+TEST_CASE("hat jo", "[cool]") {
   WordToolbox test("asd fgh");
   REQUIRE(test.isAnAnagram("das    ghf") == true);
+}
+
+TEST_CASE("set and get", "[set_get]") {
+  WordToolbox test2("asd");
+  REQUIRE(test2.getS() == test2.setS("das"));
 }
 
 #endif
