8b2206b7796f492484dbd7aa33625f34f3fc3038
nmate91 <nmate91@gmail.com>
2016-12-21 11:07:22 +0100

    my game engine refactored

--- week_9/12-21/gomoku_main.cpp
+++ week_9/12-21/gomoku_main.cpp
@@ -6,18 +6,6 @@ using namespace std;
 int main(int argc, char **argv) {
   SDL_Init(SDL_INIT_EVERYTHING);
   Context context(640, 480);
-  if(window == NULL) {
-    cout << "There was error init." << endl << SDL_GetError();
-  }
-  bool running = true;
-  SDL_Event event;
-  while (running) {
-    while (SDL_PollEvent(&event)) {
-      if(event.type == SDL_QUIT) {
-        running = false;
-        break;
-      }
-    }
-  }
+  context.display_context();
   return 0;
 }
--- week_9/12-21/my_game_engine.hpp
+++ week_9/12-21/my_game_engine.hpp
@@ -8,22 +8,41 @@
 #include <vector>
 #include <time.h>
 
+using namespace std;
+
 class Context {
 private:
-  SDL_WINDOW* screen;
+  SDL_Window* screen;
 public:
   Context(unsigned int width, unsigned int height);
   ~Context();
-
+  void display_context();
 
 };
 
 Context::Context(unsigned int width, unsigned int height) {
-  screen = SDL_CreateWindow("Bomoku", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_RESIZABLE);
+  screen = SDL_CreateWindow("Gomoku", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_RESIZABLE);
 }
 
 Context::~Context() {
   SDL_DestroyWindow(screen);
+}
+
+void Context::display_context() {
+  if(screen == NULL) {
+    cout << "There was error init." << endl << SDL_GetError();
+  }
+  bool running = true;
+  SDL_Event event;
+  while (running) {
+    while (SDL_PollEvent(&event)) {
+      if(event.type == SDL_QUIT) {
+        running = false;
+        break;
+      }
+    }
+  }
+  return;
 }
 
 #endif
