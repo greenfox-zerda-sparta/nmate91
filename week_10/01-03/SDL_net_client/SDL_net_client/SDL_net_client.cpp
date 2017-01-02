c45a5461ce36c8ed3d9ed13bc3ff7257b34ea8bd
Máté Németh <nmate91@gmail.com>
2017-01-03 00:37:10 +0100

    client added

---
+++ week_10/01-03/SDL_net_client/SDL_net_client/SDL_net_client.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include <iostream>
+#include <SDL.h>
+#include <SDL_net.h>
+
+int main(int argc, char *args[]) {
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDLNet_Init();
+  IPaddress ip;
+  SDLNet_ResolveHost(&ip, "127.0.0.1", 1234);
+  TCPsocket client = SDLNet_TCP_Open(&ip);
+  char text[100];
+  SDLNet_TCP_Recv(client, text, 100);
+  std::cout << text;
+  SDLNet_TCP_Close(client);
+  SDL_Quit();
+  return 0;
+}
