9193b411b0d26fc56cd4262784968c7d34af0087
Máté Németh <nmate91@gmail.com>
2017-01-03 14:09:26 +0100

    ex 01 - 1 client can get a message from server

--- week_10/01-03/SDL_net_basic/SDL_net_basic/SDL_net_basic.cpp
+++ week_10/01-03/SDL_net_basic/SDL_net_basic/SDL_net_basic.cpp
@@ -1,7 +1,9 @@
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 #include <SDL.h>
 #include <SDL_net.h>
+#include <istream>
 
 int main(int argc, char *args[]) {
   SDL_Init(SDL_INIT_EVERYTHING);
@@ -10,11 +12,13 @@ int main(int argc, char *args[]) {
   SDLNet_ResolveHost(&ip, NULL, 1234);
   TCPsocket server = SDLNet_TCP_Open(&ip);
   TCPsocket client;
-  const char* text = "Alma";
+  std::string text;
+  std::cout << "Write a message!" << std::endl;
+  std::getline(std::cin, text);
   while (1) {
     client = SDLNet_TCP_Accept(server);
     if (client) {
-      SDLNet_TCP_Send(client, text, 5);
+      SDLNet_TCP_Send(client, text.c_str(), text.length() + 1);
       SDLNet_TCP_Close(client);
       break;
     }
--- week_10/01-03/SDL_net_client/SDL_net_client/SDL_net_client.cpp
+++ week_10/01-03/SDL_net_client/SDL_net_client/SDL_net_client.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 #include <SDL.h>
 #include <SDL_net.h>
 
@@ -10,7 +11,7 @@ int main(int argc, char *args[]) {
   SDLNet_ResolveHost(&ip, "127.0.0.1", 1234);
   TCPsocket client = SDLNet_TCP_Open(&ip);
   char text[100];
-  SDLNet_TCP_Recv(client, text, 100);
+  SDLNet_TCP_Recv(client, text, sizeof(text));
   std::cout << text;
   SDLNet_TCP_Close(client);
   SDL_Quit();
