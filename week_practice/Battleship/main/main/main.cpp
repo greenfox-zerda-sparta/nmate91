785d96ed920f77c1686b3bd158187a3a959c3340
Máté Németh <nmate91@gmail.com>
2017-01-10 22:08:26 +0100

    test asserts added

--- week_practice/Battleship/main/main/main.cpp
+++ week_practice/Battleship/main/main/main.cpp
@@ -23,14 +23,37 @@ TEST_CASE("Is vector ready") {
 }
 
 TEST_CASE("Is vector coordinate 0") {
+  srand(time(NULL));
   Board board;
   REQUIRE(board.getVectorElementByCoordinate(5, 5) == 0);
 }
 
 TEST_CASE("Is i, j coordinate setted to 1") {
+  srand(time(NULL));
   Board board;
   board.setCoordinateToShip(5, 5, 1);
   REQUIRE(board.getVectorElementByCoordinate(5, 5) == 1);
+}
+
+TEST_CASE("Generate x") {
+  srand(time(NULL));
+  Board board;
+  REQUIRE(board.generateX(CARRIER) <= board.getVector().size() - CARRIER);
+  REQUIRE(board.generateX(CARRIER) >= 0);
+}
+
+TEST_CASE("Generate y") {
+  srand(time(NULL));
+  Board board;
+  REQUIRE(board.generateY(CARRIER) <= board.getVector()[CARRIER].size() - CARRIER);
+  REQUIRE(board.generateY(CARRIER) >= 0);
+}
+
+TEST_CASE("row free") {
+  srand(time(NULL));
+  Board board;
+  REQUIRE(board.isColFree(board.generateX(CARRIER), board.generateY(CARRIER), CARRIER));
+  REQUIRE(board.isRowFree(board.generateX(CARRIER), board.generateY(CARRIER), CARRIER));
 }
 
 TEST_CASE("Put down ship") {
@@ -42,6 +65,9 @@ TEST_CASE("Put down ship") {
   board.placeShips(SUBMARINE);
   board.placeShips(DESTROYER);
   REQUIRE(board.getVector().size());
+
+  //just for demonstration purposes:
+
   for (int i = 0; i < board.getVector().size(); i++) {
     for (int j = 0; j < board.getVector()[i].size(); j++) {
       std::cout << board.getVectorElementByCoordinate(i, j) << " ";
