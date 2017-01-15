55c62deb64d054f069eac2f8c091c34384874147
Máté Németh <nmate91@gmail.com>
2017-01-15 01:32:29 +0100

    C# practice started

---
+++ week_practice/csharp_practice/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static void printStringElements(string[] myarray)
+        {
+            foreach (string element in myarray)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Class1 newclass = new Class1();
+			newclass.Name = "anyad";
+			newclass.writesomething();
+			string[] myarray = new string[] { "bela", "sanyi" };
+            printStringElements(myarray);
+        }
+    }
+}
