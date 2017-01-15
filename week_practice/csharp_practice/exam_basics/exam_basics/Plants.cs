82f56d95791e5f9b7faa5d54388d74a303b05c56
Máté Németh <nmate91@gmail.com>
2017-01-15 01:42:01 +0100

    exam_basics_cs

---
+++ week_practice/csharp_practice/exam_basics/exam_basics/Plants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_basics
+{
+	public abstract class Plants
+	{
+		private double current_water_amount;
+		private const double max_water_amount = 70;
+		private bool needs_water;
+
+		public Plants()
+		{
+			current_water_amount = 70;
+		}
+
+		public bool needsWater
+		{
+			get { return current_water_amount < 6; }
+			set { needs_water = value; }
+		}
+
+		public virtual double water { get; set; }
+	}
+}
---
+++ week_practice/csharp_practice/exam_basics/exam_basics/Tree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_basics
+{
+	public class Tree : Plants
+	{
+		private double current_water_amount;
+		private const double max_water_amount = 40;
+		private bool needs_water;
+
+		public Tree()
+		{
+			current_water_amount = 70;
+		}
+
+		public override double water
+		{
+			get { return current_water_amount; }
+			set { current_water_amount += value * 0.4; }
+		}
+	}
+}
