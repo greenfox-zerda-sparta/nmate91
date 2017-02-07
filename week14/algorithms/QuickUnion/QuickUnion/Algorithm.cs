89942e07643c121ede1b566037f6b3b5a46791ba
Máté Németh <nmate91@gmail.com>
2017-02-07 17:06:36 +0100

    quickunion not ready

---
+++ week14/algorithms/QuickUnion/QuickUnion/Algorithm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickUnion
+{
+    public class Algorithm
+    {
+        private int[] localArray;
+
+        private int Root(int i)
+        {
+            while (i != localArray[i])
+            {
+                localArray[i] = localArray[localArray[i]];
+                i = localArray[i];
+            }
+            return i;
+        }
+
+        public Algorithm(int[] inputArray)
+        {
+            localArray = inputArray;
+        }
+
+        public bool Find(int p, int q)
+        {
+            return Root(p) == Root(q);
+        }
+
+        public void Unite(int p, int q)
+        {
+            int i = Root(p);
+            int j = Root(q);
+            localArray[i] = j;
+        }
+    }
+}
---
+++ week14/algorithms/QuickUnion/QuickUnion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[10];
+            for(int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = i;
+            }
+
+            Algorithm quickUniion = new Algorithm(myArray);
+            Console.WriteLine(quickUniion.Find(1, 3).ToString());
+            quickUniion.Unite(1, 3);
+            Console.WriteLine(quickUniion.Find(1, 3).ToString());
+            quickUniion.Unite(4, 1);
+            Console.WriteLine(quickUniion.Find(4, 3).ToString());
+        }
+    }
+}
