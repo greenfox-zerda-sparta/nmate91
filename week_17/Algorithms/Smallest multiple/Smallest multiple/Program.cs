fb2e11506f2036c725634f0a40d7e6ce928238cf
Máté Németh <nmate91@gmail.com>
2017-02-21 14:10:37 +0100

    SmallestMultiple added

---
+++ week_17/Algorithms/Smallest multiple/Smallest multiple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smallest_multiple
+{
+    class Program
+    {
+        static bool IsDivisible(int number, int to)
+        {
+            for(int i = to; i > 1; i--)
+            {
+                if(number % i != 0)
+                {
+                    return false;
+                }
+             }
+            return true;
+        }
+        static int SmallestMultiple()
+        {
+            int count = 10;
+            while (true)
+            {
+                if(IsDivisible(count, 20))
+                {
+                    return count;
+                }
+                count++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SmallestMultiple());
+        }
+    }
+}
