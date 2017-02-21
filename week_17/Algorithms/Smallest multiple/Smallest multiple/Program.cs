079a699c86524c6f34c606cefd1d281afded8c45
Máté Németh <nmate91@gmail.com>
2017-02-21 14:57:35 +0100

    largest prime factor

--- week_17/Algorithms/Smallest multiple/Smallest multiple/Program.cs
+++ week_17/Algorithms/Smallest multiple/Smallest multiple/Program.cs
@@ -31,9 +31,35 @@ namespace Smallest_multiple
                 count++;
             }
         }
+        static bool IsPrime(double number)
+        {
+            for(double i = 2; i < number; i++)
+            {
+                if(number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static double ReturnLargestPrimeFactor(double number)
+        {
+            double i;
+            double largest = number;
+            for (i = 2; i < Math.Sqrt(number) / 2; i++)
+            {
+                if(number % i == 0 && IsPrime(i))
+                {
+                    largest = i;
+                }
+            }
+            return largest;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine(SmallestMultiple());
+            //Console.WriteLine(SmallestMultiple());
+            Console.WriteLine(ReturnLargestPrimeFactor(600851475143));
         }
     }
 }
